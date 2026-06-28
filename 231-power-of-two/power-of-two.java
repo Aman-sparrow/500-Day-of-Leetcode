9880f606446a641f2af260d10758125093e27e18
Aman Singh <121784478+Aman-sparrow@users.noreply.github.com>
2026-06-29 01:04:19 +0530

    Time: 1 ms (96.46%) | Memory: 42.5 MB (72.16%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPowerOfTwo(int num) {
+        if(num<=0){return false;}
+        else if(num==1){
+            return true;
+        }
+        else{
+            while(num%2 == 0){
+                num=num/2;
+            }
+            if(num==1){
+                return true;
+            }
+            else{return false;}
+        }
+        
+    }
+}
